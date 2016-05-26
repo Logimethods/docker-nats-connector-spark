53e47417d84d8e36fba45a32febe16feee3d1196
Laurent Magnin <laugimethods@Huard.local>
2016-05-26 10:37:11 -0400

    println("System properties: " + System.getProperties())

--- inject/target/docker/4/user-files/simulations/nats/NatsInjection.scala
+++ inject/target/docker/4/user-files/simulations/nats/NatsInjection.scala
@@ -22,6 +22,7 @@ class NatsInjection extends Simulation {
   
   val properties = new Properties()
   properties.setProperty(io.nats.client.Constants.PROP_URL, "nats://nats-main:4222")
+  println("System properties: " + System.getProperties())
   val subject = System.getProperties().getProperty("GATLING.TO_NATS.SUBJECT", "fromGatling")
   println("Will emit messages to " + subject)
   val natsProtocol = NatsProtocol(properties, subject)
