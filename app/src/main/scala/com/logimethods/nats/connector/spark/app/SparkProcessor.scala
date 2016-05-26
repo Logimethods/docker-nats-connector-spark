4330c878345006a118fb600c261f0e192f8c1644
Laurent Magnin <laugimethods@Huard.local>
2016-05-26 16:00:13 -0400

    properties.put(PROP_URL, "nats://nats-main:4222")

--- app/src/main/scala/com/logimethods/nats/connector/spark/app/SparkProcessor.scala
+++ app/src/main/scala/com/logimethods/nats/connector/spark/app/SparkProcessor.scala
@@ -15,19 +15,30 @@ import org.apache.spark.SparkContext
 import org.apache.spark.streaming.Duration
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.storage.StorageLevel;
+import io.nats.client.Constants._
 
 import com.logimethods.nats.connector.spark.NatsToSparkConnector
 
 object SparkProcessor extends App {
-		val conf = new SparkConf().setAppName("NATS Data Processing").setMaster("local[2]");
-		val sc = new SparkContext(conf);
-		val ssc = new StreamingContext(sc, new Duration(200));
-		
-		val properties = new Properties();
-		val messages = ssc.receiverStream(NatsToSparkConnector.receiveFromNats(properties, StorageLevel.MEMORY_ONLY, "INPUT"))
-		
-		val integers = messages.map({ str => Integer.parseInt(str) })
-		val max = integers.reduce({ (int1, int2) => Integer.max(int1, int2) })
-		
-		max.print()
+	System.setProperty("org.slf4j.simpleLogger.log.com.logimethods.nats.connector.spark.NatsToSparkConnector", "trace");
+	
+  val inputSubject = args(0)
+  val outputSubject = args(1)
+  println("Will process messages from " + inputSubject + " to " + outputSubject)
+
+  val conf = new SparkConf().setAppName("NATS Data Processing").setMaster("local[2]");
+  val sc = new SparkContext(conf);
+  val ssc = new StreamingContext(sc, new Duration(200));
+
+  val properties = new Properties();
+  properties.put("servers", "nats://nats-main:4222")
+  properties.put(PROP_URL, "nats://nats-main:4222")
+  val messages = ssc.receiverStream(NatsToSparkConnector.receiveFromNats(properties, StorageLevel.MEMORY_ONLY, "INPUT"))
+
+  val integers = messages.map({ str => Integer.parseInt(str) })
+  val max = integers.reduce({ (int1, int2) => Integer.max(int1, int2) })
+
+  max.print()
+  
+  ssc.start();		
 }
