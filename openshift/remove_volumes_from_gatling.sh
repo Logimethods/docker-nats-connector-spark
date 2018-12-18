fde99efa335da741b20b8f7e515ad5dca401cd86
Laurent Magnin <laurent.magnin@logimethods.com>
2018-12-18 13:35:55 -0500

    curl -o docker-copyedit.py

--- openshift/remove_volumes_from_gatling.sh
+++ openshift/remove_volumes_from_gatling.sh
@@ -2,7 +2,11 @@
 
 # https://github.com/gdraheim/docker-copyedit
 
-./docker-copyedit.py \
- FROM denvazh/gatling:2.2.2 INTO logimethods/gatling:2.2.2 REMOVE ALL VOLUMES
+curl -o docker-copyedit.py https://raw.githubusercontent.com/gdraheim/docker-copyedit/master/docker-copyedit.py
+chmod +x docker-copyedit.py
+
+./docker-copyedit.py FROM denvazh/gatling:2.2.2 INTO logimethods/gatling:2.2.2 rm all volumes
+#REMOVE ALL VOLUMES
+docker image inspect logimethods/gatling:2.2.2 | grep Volumes
 
 docker push logimethods/gatling:2.2.2
