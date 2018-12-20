ce992b5cfde0cf9db1e270c1b1278cb017dda712
Laurent Magnin <laurent.magnin@logimethods.com>
2018-12-20 09:58:27 -0500

    main-app

--- app/src/main/scala/com/logimethods/nats/connector/spark/app/SparkProcessor.scala
+++ app/src/main/scala/com/logimethods/nats/connector/spark/app/SparkProcessor.scala
@@ -23,10 +23,12 @@ import org.apache.log4j.{Level, LogManager, PropertyConfigurator}
 import com.logimethods.connector.nats.to_spark._
 import com.logimethods.scala.connector.spark.to_nats._
 
+import org.apache.hadoop.security.UserGroupInformation
+
 object SparkProcessor extends App {
   val log = LogManager.getRootLogger
   log.setLevel(Level.WARN)
-  
+
   Thread.sleep(5000)
 
   val inputSubject = args(0)
@@ -34,17 +36,21 @@ object SparkProcessor extends App {
   val outputSubject = args(1)
   val outputStreaming = outputSubject.toUpperCase.contains("STREAMING")
   println("Will process messages from " + inputSubject + " to " + outputSubject)
-  
+
   val logLevel = scala.util.Properties.envOrElse("LOG_LEVEL", "INFO")
   println("LOG_LEVEL = " + logLevel)
 
   val sparkMasterUrl = System.getenv("SPARK_MASTER_URL")
   println("SPARK_MASTER_URL = " + sparkMasterUrl)
   val conf = new SparkConf().setAppName("NATS Data Processing").setMaster(sparkMasterUrl);
+
+  // https://stackoverflow.com/questions/41864985/hadoop-ioexception-failure-to-login
+  UserGroupInformation.setLoginUser(UserGroupInformation.createRemoteUser("sparkuser"));
+  
   val sc = new SparkContext(conf);
 //  val jarFilesRegex = "java-nats-streaming-(.*)jar|guava(.*)jar|protobuf-java(.*)jar|jnats-(.*)jar|nats-connector-spark-(.*)jar|docker-nats-connector-spark(.*)jar"
   val jarFilesRegex = "(.*)jar"
-  for (file <- new File("/app/").listFiles.filter(_.getName.matches(jarFilesRegex))) 
+  for (file <- new File("/app/").listFiles.filter(_.getName.matches(jarFilesRegex)))
     { sc.addJar(file.getAbsolutePath) }
   val ssc = new StreamingContext(sc, new Duration(2000));
 
@@ -53,9 +59,9 @@ object SparkProcessor extends App {
   println("NATS_URI = " + natsUrl)
   properties.put("servers", natsUrl)
   properties.put(PROP_URL, natsUrl)
-  
+
   val clusterId = System.getenv("NATS_CLUSTER_ID")
-  
+
   val integers =
     if (inputStreaming) {
       NatsToSparkConnector
@@ -70,10 +76,10 @@ object SparkProcessor extends App {
         .withSubjects(inputSubject)
         .asStreamOf(ssc)
     }
-  
+
   val max = integers.reduce({ (int1, int2) => Math.max(int1, int2) })
 
-  if (logLevel.equals("DEBUG")) { 
+  if (logLevel.equals("DEBUG")) {
     max.print()
   }
 
@@ -88,8 +94,8 @@ object SparkProcessor extends App {
                             .withSubjects(outputSubject)
                             .publishToNats(max)
   }
-  
-  ssc.start();		
-  
+
+  ssc.start();
+
   ssc.awaitTermination()
 }
